1ef8c5843889c51e9db3082c88cc878db90b636f
shanon <13fritsh@gmail.com>
2020-07-07 17:43:55 -0500

    added my components and worked in my app.js adding state and axios

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {v4 as uuid} from 'uuid';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    myInfo: [],
+    followers: [],
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/sfritz24')
+      .then(response =>{
+        this.setState({
+          myInfo: response.data
+        })
+      })
+      .catch(error =>{
+        console.log('this is the error for myInfo:', error)
+      })
+
+    axios.get('https://api.github.com/users/sfritz24/followers')
+      .then(response =>{
+        this.setState({
+          followers: response.data
+        })
+      })
+      .catch(error =>{
+        console.log('this is the error for followers:', error)
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Welcome to your Todo App!</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
